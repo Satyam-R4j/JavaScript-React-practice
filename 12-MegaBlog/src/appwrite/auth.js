f3ea1ae9fe20f312e40cbb9d90752d852907579d
Satyam-R4j <satyamraj418@gmail.com>
2025-03-13 19:41:05 +0530

    Coding AppWriter functionality

---
+++ 12-MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,82 @@
+import conf from "../conf/conf";
+
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService
+{
+    Client = new Client()
+    account
+
+    constructor()
+    {
+        this.Client.setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.Client)
+    }
+
+    async createAccount(email, password, name)
+    {
+        // eslint-disable-next-line no-useless-catch
+        try
+        {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount)
+            {
+                //call method
+                return this.login(email, password)
+            } else
+            {
+                return userAccount
+            }
+        }
+        catch (error)
+        {
+            throw error
+        }
+    }
+
+    async login({ email, password })
+    {
+        // eslint-disable-next-line no-useless-catch
+        try
+        {
+            return await this.account.createEmailPasswordSession(email, password)
+        }
+        catch (error)
+        {
+            throw error
+        }
+    }
+
+    async getCurrentUser()
+    {
+        // eslint-disable-next-line no-useless-catch
+        try
+        {
+            return await this.account.get()
+        } catch (error)
+        {
+            throw error
+        }
+        return null
+    }
+
+    async logout()
+    {
+        // eslint-disable-next-line no-useless-catch
+        try
+        {
+            await this.Client.account.deleteSessions()
+        } catch (error)
+        {
+            throw (error)
+        }
+    }
+
+}
+
+
+const authService = new AuthService()
+
+export default AuthService
