4371da561c24db193f8cd7dce3d499c4f3597c27
Satyam-R4j <satyamraj418@gmail.com>
2025-03-16 20:39:31 +0530

    Working on Front-end

--- 12-MegaBlog/src/App.jsx
+++ 12-MegaBlog/src/App.jsx
@@ -1,15 +1,44 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import  authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-import './App.css'
+
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  console.log()
-  return (
-    
-    <>
-      <h1>A blog app with appwrite </h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) =>{
+      if(userData)
+      {
+        dispatch(login(userData))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, []);
+
+  return  !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header/>
+        <main>
+        TODO:  {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+  
 }
 
-export default App
+export default App;
