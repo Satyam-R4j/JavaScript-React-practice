5dfb75f395423419cfb411852f5c1fa9a432d8f2
Satyam-R4j <satyamraj418@gmail.com>
2025-03-18 03:57:35 +0530

    Working on Front-End

---
+++ 12-MegaBlog/src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+import authService from "../appwrite/auth";
+import {Link,useNavigate} from "react-router-dom";
+
+
+
+const Signup = () => {
+  return <div>Signup</div>;
+};
+
+export default Signup;
